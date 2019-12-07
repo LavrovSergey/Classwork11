59be18b3758c56a65cf7b9c7190d758ce0d33e2c
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2019-12-08 01:06:19 +0200

    1

--- Classwork11/Classwork11.cpp
+++ Classwork11/Classwork11.cpp
@@ -1,8 +1,17 @@
 ﻿#include <iostream>
 #include <conio.h>
 #include <fstream>
+#include <cctype> 
 using namespace std;
 int show(), light(), count(); void menu(); 
+int back()
+{   
+	cout << "Нажмите пробел, что б вернутся в меню";
+	int code;
+	code = _getch();
+	if (code == 32) { menu(); }
+	else return 0;
+}
 int main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -29,25 +38,90 @@ int show(){
 	system("cls");
 	char s[70];
 	ifstream f("text.txt");
+	f.seekg(0, ios::beg);
 	while (!f.eof()) 
 	{
 		f.getline(s, 70);
-		cout << s<<endl;
+		cout <<s<<endl;
 	}
 	f.close();
-	cout << "Нажмите пробел, что б вернутся в меню";
-	int code;
-	code = _getch();
-	if (code == 32) { menu(); }
-	else return 0;
+	
+	back();
+	return 0;
 }
-int light() {
+int light(){ 
+	int codee;
 	system("cls");
-
+	char text[1000] = {'\0'};
+	ifstream f("text.txt");
+	f.read(text, 1000);
+	for (int i = 0; i < 1000; i++)
+	{
+		if (isalpha(text[i]))
+		{ 
+			text [i]=tolower(text[i]); 
+		}
+	}
+	
+	for (int i = 0; i < 1000; i++)
+	{
+		for (int j = i + 1; j < 1000; j++)
+		{
+			if ((isalpha(text[i]) && isalpha(text[j])) && (text[i] == text[j]) && ((text[j + 1] == ' ' || text[j + 1] == ',' || text[j + 1] == '\0' || text[j + 1] == '.' || text[j + 1] == '!' || text[j + 1] == '?' || text[j + 1] == '\n') && (text[i] >= 66 || text[i] <= 90 || text[i - 1] == '\n' || text[i - 1] == ' ' || !isalpha(i-1))))
+			{
+				cout << "Нажмите любую клавишу, если хотите выделить следующее слово. Нажмите SPACE, если хотите обратно"<<endl;
+				codee = _getch();
+				if (codee != 32)
+				{
+					system("cls");
+					text[i] = toupper(text[i]);
+					text[j] = toupper(text[j]);
+					for (int a = 0; a < 1000; a++)
+					{
+						cout << text[a];
+					}
+					i =+j;
+					text[i] = tolower(text[i]);
+					text[j] = tolower(text[j]);
+					break;
+				}
+				else menu();
+			}
+			else if (text[j + 1] == ' ' || text[j + 1] == ',' || text[j + 1] == '.' || text[j + 1] == '!' || text[j + 1] == '?' || text[j + 1] == '\n') { break; }
+		}
+	}
+	back();
 	return 0;
 }
 int count() {
 	system("cls");
+	int count=0;
+	char text[1000] = { '\0' };
+	ifstream f("text.txt");
+	f.read(text, 1000);
+	for (int i = 0; i < 1000; i++)
+	{
+		if (isalpha(text[i]))
+		{
+			text[i] = tolower(text[i]);
+		}
+	}
 
+	for (int i = 0; i < 1000; i++)
+	{
+		for (int j = i + 1; j < 1000; j++)
+		{
+			if ((isalpha(text[i]) && isalpha(text[j])) && (text[i] == text[j]) && ((text[j + 1] == ' ' || text[j + 1] == ',' || text[j + 1] == '\0' || text[j + 1] == '.' || text[j + 1] == '!' || text[j + 1] == '?' || text[j + 1] == '\n') && (text[i] >= 66 || text[i] <= 90 || text[i - 1] == '\n' || text[i - 1] == ' ' || !isalpha(i - 1))))
+			{
+				i =+ j;
+				count =count+ 1;
+					break;
+			}
+			
+			else if (text[j + 1] == ' ' || text[j + 1] == ',' || text[j + 1] == '.' || text[j + 1] == '!' || text[j + 1] == '?' || text[j + 1] == '\n') { break; }
+		}
+	}
+	cout << count << " ---- вот столько слов, которые начинаются и заканчиваются на одну и туже букву" << endl;
+	back();
 	return 0;
 }
